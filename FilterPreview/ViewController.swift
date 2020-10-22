7aba389581356fd83541b12dc81474b9e00c57d4
Vikash Hart <vikashhart@gmail.com>
2020-10-21 21:28:48 -0400

    Repo setup & SceneDelegate removal

--- FilterPreview/ViewController.swift
+++ FilterPreview/ViewController.swift
@@ -1,20 +1,8 @@
-//
-//  ViewController.swift
-//  FilterPreview
-//
-//  Created by Vikash Hart on 10/21/20.
-//  Copyright © 2020 Vikash Hart. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
