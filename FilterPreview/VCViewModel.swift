6bb52bdcf925b591a37a8b579ab511ffc0d1847c
Vikash Hart <vikashhart@gmail.com>
2020-10-29 15:49:05 -0400

    filter guide

--- FilterPreview/VCViewModel.swift
+++ FilterPreview/VCViewModel.swift
@@ -6,23 +6,27 @@ protocol VCViewModeling {
     var numberOfCells: CGFloat { get }
 
     var selectedIndex: IndexPath { get set }
+
+    var filters: [FilterObject] { get }
 }
 
 class VCViewModel: VCViewModeling {
     var cellSpacing: CGFloat
-
     var numberOfSpaces: CGFloat
-
     var numberOfCells: CGFloat
 
     var selectedIndex: IndexPath
 
+    var filters: [FilterObject]
+
     init(cellSpacing: CGFloat = 10,
          numberOfCells: CGFloat = 1,
-         selectedIndex: IndexPath = IndexPath(row: 0, section: 0)) {
+         selectedIndex: IndexPath = IndexPath(row: 0, section: 0),
+         filters: FilterGuide = FilterGuide()) {
         self.cellSpacing = cellSpacing
         self.numberOfCells = numberOfCells
         self.numberOfSpaces = numberOfCells + 1
         self.selectedIndex = selectedIndex
+        self.filters = filters.filters
     }
 }
